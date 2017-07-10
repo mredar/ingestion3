f035b6ef79331c8733341796976aa1749fbd08d7
Scott Williams <scott@dp.la>
2017-07-09 22:43:07 -0400

    Initial stab at lexvo enrichment based on the DcmiType enrichment

--- src/main/scala/dpla/ingestion3/enrichments/VocabEnforcer.scala
+++ src/main/scala/dpla/ingestion3/enrichments/VocabEnforcer.scala
@@ -1,11 +1,17 @@
 package dpla.ingestion3.enrichments
 
-import dpla.ingestion3.mappers.rdf.DCMIType
+import dpla.ingestion3.mappers.rdf.{DCMIType, ISO_639_3}
+
+import scala.io.Source
 
 
 trait VocabEnforcer[T] {
   val enforceVocab: (T, Set[T]) => Boolean = (value, vocab) => {
     vocab.contains(value)
+  }
+
+  val matchAbbvToTerm: (T, Map[T,T]) => String = (value, vocab) => {
+    vocab.getOrElse(value, value)
   }
 }
 
@@ -28,4 +34,24 @@ object DcmiTypeEnforcer extends VocabEnforcer[String] {
   ).map(_.getLocalName)
 
   val enforceDcmiType: (String) => Boolean = enforceVocab(_, DcmiTypeStrings)
+}
+
+object LexvoEnforcer extends VocabEnforcer[String] {
+  val iso_639_3 = ISO_639_3()
+
+  val lexvoStrings = {
+    // TODO make this a config property
+    val bufferedSource = Source.fromFile("./data/iso-639-3/iso-639-3.tab")
+    val lines = bufferedSource.getLines
+    bufferedSource.close
+
+    lines
+      .map(_.split("\t"))
+      .map(f => (f(0), f(1)))
+      .toMap
+  }
+
+  val enforceLexvoType: (String) => String = matchAbbvToTerm(_, lexvoStrings)
+
+
 }
